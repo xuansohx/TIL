71b504e385d514a83cbf172dfd5754acd4eaf064
An-Sohyoun <ansoh94@naver.com>
2019-09-18 23:17:35 +0900

    Android Example code - use GoogleMap API

---
+++ 예제코드/day84_Android_MapAPI_Example/p675/app/src/main/java/com/example/p675/MainActivity.java
@@ -0,0 +1,154 @@
+package com.example.p675;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.PermissionChecker;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MainActivity extends AppCompatActivity {
+    SupportMapFragment mapFragment;
+    GoogleMap map;
+    TextView textView;
+
+    MarkerOptions myLocationMarker;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textView = findViewById(R.id.textView);
+
+        // mapFragment 가져옴
+        mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(GoogleMap googleMap) {
+                Log.d("Map", "지도 준비됨.");
+                map = googleMap; // OS에서 제공하는 지도 객체 가져옴
+                /*map.setMaxZoomPreference(15);*/
+                map.setMyLocationEnabled(true);
+                startLocationService();
+
+            }
+        });
+
+        // 초기화 작업
+        try {
+            MapsInitializer.initialize(this);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        // Permission Check를 위한 준비
+        String[] Permissions = {
+                Manifest.permission.ACCESS_FINE_LOCATION, // 필요한 permission 여기에 기입
+        };
+
+        ActivityCompat.requestPermissions(this, Permissions, 101);
+
+        // permission 물어보기
+
+        int check = PermissionChecker.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION);
+
+        if (check == PackageManager.PERMISSION_GRANTED) {
+            Toast.makeText(this,"ACCEPT",Toast.LENGTH_SHORT).show();
+            //startLocationService();
+            return;
+        } else {
+            Toast.makeText(this, "DENY", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+    } // end onCreate
+
+    private void showCurrentLocation(Double latitude, Double longitude) {
+        // 위도와 경도를 넣어주면 latitude, longitude 객체를 만들어 지도를 이동
+        LatLng curPoint = new LatLng(latitude, longitude);
+        map.animateCamera(CameraUpdateFactory.newLatLngZoom(curPoint, 15));
+        showMyLocationMarker(curPoint);
+    }
+
+    private void showMyLocationMarker(LatLng curPoint) {
+        if (myLocationMarker == null) {
+            myLocationMarker = new MarkerOptions();
+            myLocationMarker.position(curPoint);
+            myLocationMarker.title("● 내 위치\n");
+            myLocationMarker.snippet("● GPS로 확인한 위치");
+            myLocationMarker.icon(BitmapDescriptorFactory.fromResource(R.drawable.mylocation)); // 아이콘 이미지
+            map.addMarker(myLocationMarker);
+        } else {
+            myLocationMarker.position(curPoint);
+        }
+    }
+
+    public void startLocationService() {
+        // LogationManager 만듦
+        LocationManager manager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+        // Manager가 현재 위치 찾아 → 경도, 위도 가져옴
+        try {
+            Location location = manager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+            if (location != null) {
+                double latitude = location.getLatitude();
+                double longitude = location.getLongitude();
+                String message = "" + latitude + ":" + longitude;
+                showCurrentLocation(latitude, longitude);
+                textView.setText(message);
+            }
+
+            // Listener에 의해 실시간으로 가져옴
+            GPSListener gpsListener = new GPSListener();
+            long minTime = 1; // 10초에 한 번씩 바뀐 정보를 수신
+            float minDistance = 0;
+
+            /// Manager가 현재 위치 바뀌는 것 감지하여 반영
+            manager.requestLocationUpdates(
+                    LocationManager.GPS_PROVIDER,
+                    minTime, minDistance, gpsListener);
+            // 네 개의 정보가 들어감
+            // gpsListener의 역할? 일정시간마다 호출되어 바뀐 값을 찾아아
+            Toast.makeText(getApplicationContext(), "내 위치확인 요청함",
+                    Toast.LENGTH_SHORT).show();
+
+        } catch(SecurityException e) {
+            e.printStackTrace();
+        }
+    }
+
+    class GPSListener implements LocationListener {
+        public void onLocationChanged(Location location) {
+            Double latitude = location.getLatitude();
+            Double longitude = location.getLongitude();
+            showCurrentLocation(latitude, longitude);
+            String message = "latitude : "+ latitude + " longitude : "+ longitude;
+            textView.setText(message);
+        }
+
+        public void onProviderDisabled(String provider) { }
+
+        public void onProviderEnabled(String provider) { }
+
+        public void onStatusChanged(String provider, int status, Bundle extras) { }
+    }
+
+
+}
